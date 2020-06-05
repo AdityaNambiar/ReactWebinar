fc77dc4dfca64f72c7c33cbdc0096657bfa6af79
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-06-05 20:13:09 +0530

    Placed EventListItem while preserving ELI component as before

--- src/components/EventListGroup.jsx
+++ src/components/EventListGroup.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import EventListItem from './EventListItem';
+//import EventListItem from './EventListItem';
+import '../styles/eventlisting.css';
 
 const divStyle = {
 	display: 'flex',
@@ -26,6 +27,20 @@ function EventListGroup(props){
 			}
 		</div>
 	);
+}
+
+class EventListItem extends React.Component {
+	render() {
+		const eventName = this.props.eventName;
+		const { deleteEventHandler } = this.props;
+		//const { eventName } = this.state; // Object destructing (Benefit of this syntax is you get to avoid writing "this.state" if you have multiple states)
+		return(
+			<div className="itemCont">
+			  <span>{ eventName }</span>
+			  <span onClick={() => deleteEventHandler(eventName)}>&#x2715;</span>
+			</div>
+		);
+	}
 }
 
 export default EventListGroup;
